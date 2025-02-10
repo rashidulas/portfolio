ffcf1a16d31ab5b1116e1906e8e002d1c9a99d59
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-02-10 07:43:53 -0600

    Added theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#050816',
+  			secondary: '#aaa6c3',
+  			tertiary: '#151030',
+  			'black-100': '#100d25',
+  			'black-200': '#090325',
+  			'white-100': '#f3f3f3'
+  		},
+  		boxShadow: {
+  			card: '0px 35px 120px -15px #211e35'
+  		},
+  		screens: {
+  			xs: '450px'
+  		},
+  		// backgroundImage: {
+  		// 	'hero-pattern': 'url('/src/assets/herobg.png')'
+  		// },
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
