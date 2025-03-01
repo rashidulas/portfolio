4364ec936aee7e403d5827529fa5c0f8a91821a5
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-03-01 01:20:42 -0600

    adding constants for route

---
+++ constants/index.tsx
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/users.svg",
+    route: "/about",
+    label: "About Me",
+  },
+  {
+    imgURL: "/icons/star.svg",
+    route: "/contact",
+    label: "Contact",
+  },
+  {
+    imgURL: "/icons/suitcase.svg",
+    route: "/portfolio",
+    label: "Portfolio",
+  },
+];
---
+++ constants/routes.ts
@@ -0,0 +1,8 @@
+const ROUTES = {
+    HOME: "/",
+    PORFOLIO: "/portfolio",
+    ABOUT: "/about",
+    CONTACT: "/contact",
+  };
+  
+  export default ROUTES;
